38b7b703ae8aa30d4b023ad78b97035ac9ef0fbf
Ben Glass <glassb@users.noreply.github.com>
2018-08-06 15:17:28 -0400

    Add files via upload

---
+++ query.py
@@ -0,0 +1,67 @@
+import MySQLdb
+
+conn = MySQLdb.connect(host='jackrabbit', user='highpeaksdw', passwd='naivecuriousarcanebionic', db='highpeaksdw')
+cursor = conn.cursor()
+
+
+
+print('\n\n\n\nCONNECTED TO DATABASE: highpeaksdw\n\n***************************************************************************\n\n')
+
+
+#print('TEMPO Numerical Data for all 12 Townhouse suites\n\n\n\n')
+
+# ************************************ Initilize Arrays
+
+#(NCS (i.e. 1,2,3), Number suite, meter id, current, 7-day)
+Townhouses = [[1,1,1,None,None],[1,2,2,None,None],[1,2,3,None,None],[1,2,4,None,None],[2,1,5,None,None], [2,2,6,None,None],[2,3,7,None,None],[2,4,8,None,None],[3,1,9,None,None],[3,2,10,None,None],[3,3,11,None,None],[3,4,12,None,None]]
+
+for i in Townhouses:
+	if i[0] == 1:
+		i[0] = 'North'
+	elif i[0] == 2:
+		i[0] = 'Center'
+	else:
+		i[0] = 'South'
+
+# ************************************ Current kW Consumption
+for i in range(12):
+	#print(i)
+	cursor.execute("SELECT data FROM highpeaksdw.kWdata where sequenceNum = (SELECT max(sequenceNum) FROM highpeaksdw.kWdata where meterid = (%s+1))",(i,))
+	row = cursor.fetchone()
+	Townhouses[i][3] = float(row[0])
+
+
+# ************************************ 7-day Average kW Consumption
+for i in range(12):
+	cursor.execute("SELECT AVG(data) FROM (SELECT * FROM highpeaksdw.kWdata WHERE date >= DATE(NOW()) - INTERVAL 7 DAY HAVING meterid = (%s+1)) subquery",(i,))
+	row = cursor.fetchone()
+	Townhouses[i][4] = row[0]
+
+
+# ************************************ Aggregate Average kW Consumption
+cursor.execute("SELECT AVG(data) FROM highpeaksdw.kWdata")
+row = cursor.fetchone()
+print("Aggregate Average kW consumption (All 12 Suites since 07-01-2018): %s" % (round(row[0],2)))
+print("\n\n\n\n")
+
+
+conn.close()    
+
+
+for i in Townhouses:
+	print("		[Townhouse %s %s] 			Current kW: %s  		7-day Average kW: %s \n" % (i[0],i[1],i[3],round(i[4],2)))
+
+
+print("\n\n\n\n")
+
+
+
+
+
+
+#print(Townhouses)       
+                
+
+              
+
+
