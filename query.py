f3cda431c24fb9e545a4be1e18d15c5936c46680
Ben Glass <glassb@users.noreply.github.com>
2018-08-06 22:55:41 -0400

    Update query.py

--- query.py
+++ query.py
@@ -1,11 +1,16 @@
 import MySQLdb
+import datetime
+mydate = datetime.datetime.now() 
+month = mydate.strftime("%B")
 
 conn = MySQLdb.connect(host='jackrabbit', user='highpeaksdw', passwd='naivecuriousarcanebionic', db='highpeaksdw')
 cursor = conn.cursor()
 
 
 
-print('\n\n\n\nCONNECTED TO DATABASE: highpeaksdw\n\n***************************************************************************\n\n')
+print('\n\n\n\nCONNECTED TO DATABASE: highpeaksdw')
+print("TIMESTAMP: ",mydate)
+print('\n\n***************************************************************************\n\n')
 
 
 #print('TEMPO Numerical Data for all 12 Townhouse suites\n\n\n\n')
@@ -13,7 +18,7 @@ print('\n\n\n\nCONNECTED TO DATABASE: highpeaksdw\n\n***************************
 # ************************************ Initilize Arrays
 
 #(NCS (i.e. 1,2,3), Number suite, meter id, current, 7-day)
-Townhouses = [[1,1,1,None,None],[1,2,2,None,None],[1,2,3,None,None],[1,2,4,None,None],[2,1,5,None,None], [2,2,6,None,None],[2,3,7,None,None],[2,4,8,None,None],[3,1,9,None,None],[3,2,10,None,None],[3,3,11,None,None],[3,4,12,None,None]]
+Townhouses = [[1,1,1,None,None,None],[1,2,2,None,None,None],[1,2,3,None,None,None],[1,2,4,None,None,None],[2,1,5,None,None,None], [2,2,6,None,None,None],[2,3,7,None,None,None],[2,4,8,None,None,None],[3,1,9,None,None,None],[3,2,10,None,None,None],[3,3,11,None,None,None],[3,4,12,None,None,None]]
 
 for i in Townhouses:
 	if i[0] == 1:
@@ -25,7 +30,6 @@ for i in Townhouses:
 
 # ************************************ Current kW Consumption
 for i in range(12):
-	#print(i)
 	cursor.execute("SELECT data FROM highpeaksdw.kWdata where sequenceNum = (SELECT max(sequenceNum) FROM highpeaksdw.kWdata where meterid = (%s+1))",(i,))
 	row = cursor.fetchone()
 	Townhouses[i][3] = float(row[0])
@@ -38,21 +42,35 @@ for i in range(12):
 	Townhouses[i][4] = row[0]
 
 
+
+# ************************************ This-Months Average kW Consumption (so-far)
+for i in range(12):
+	cursor.execute("SELECT AVG(data) FROM highpeaksdw.kWdata where meterid = (%s+1)",(i,))
+	row = cursor.fetchone()
+	Townhouses[i][5] = row[0]
+
 # ************************************ Aggregate Average kW Consumption
 cursor.execute("SELECT AVG(data) FROM highpeaksdw.kWdata")
 row = cursor.fetchone()
-print("Aggregate Average kW consumption (All 12 Suites since 07-01-2018): %s" % (round(row[0],2)))
+print("*: %s kW" % (round(row[0],2)))
 print("\n\n\n\n")
 
 
-conn.close()    
+conn.close()  
 
+
+
+print("						Current kW  		7-day Average kW 	   %s Average kW     \n" % (month))
 
 for i in Townhouses:
-	print("		[Townhouse %s %s] 			Current kW: %s  		7-day Average kW: %s \n" % (i[0],i[1],i[3],round(i[4],2)))
+	print("		[Townhouse %s %s] 		   %s  		     %s 			 %s     \n" % (i[0],i[1],i[3],round(i[4],2),round(i[5],2)))
 
 
 print("\n\n\n\n")
+
+
+
+	
 
 
 
